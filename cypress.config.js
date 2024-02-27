e1adecfdcd7e0d49a807f3e43291353f96b12632
Kyle Dominic Adsuara <kyleadsuara@gmail.com>
2024-02-27 13:27:33 +0800

    Added necessary packages for pipeline

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'junit',
+  reporterOptions: {
+      mochaFile: "results/results-[hash].xml"
+ },
   e2e: {
     baseUrl: 'https://www.demoblaze.com/index.html',
     setupNodeEvents(on, config) {
